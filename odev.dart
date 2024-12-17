7a2b08faa0d4b5842fe1cedcccb1a2f4bdbfdeed
emrecan-nt <emremizrak150@gmail.com>
2024-12-17 21:18:42 +0300

    ödev başarıyla yapıldı

---
+++ odev.dart
@@ -0,0 +1,40 @@
+void main() {
+//ödevin A kısmı
+  int birincisayi = 23;
+  print("sayiniz: $birincisayi");
+
+  String metin = "ben emre";
+  print("metniniz:$metin");
+
+  double ondaliklisayi = 0.14;
+  print("ondalikli sayiniz:$ondaliklisayi");
+
+  bool askerlikyaptimi = false;
+  print("askerlik durumu =$askerlikyaptimi");
+
+  dynamic dinamikdeger = "flutter";
+  print("dinamik degeriniz= $dinamikdeger");
+
+  String karakter = 'e';
+  print("char degeriniz: $karakter");
+
+  // ödevin B kısmı
+  // camel case
+  var birinciSayi = 23;
+  print("sayiniz (camel case) $birinciSayi");
+  // snake_case
+  var askerlikyapti_mi = false;
+  print("askerlik durumu =$askerlikyapti_mi");
+  //paskal case
+  var DinamikDeger = "flutter";
+  print("dynamic değişkeni (PascalCase): $DinamikDeger");
+
+  // ödevin c kısmı
+  String ad = 'Emre'; 
+  String soyad = 'Mızrak'; 
+  int yas = 21; 
+  bool resitlik = true; 
+
+  print('Merhaba, ben $ad $soyad. $yas yaşındayım. Reşitlik durumum: $resitlik'); 
+
+}
